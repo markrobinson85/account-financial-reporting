c0d98cdaa9e0d157f6cd3b9010fe763b1bae4775
Mark Robinson <mark@interstyle.ca>
2018-06-29 11:13:42 -0700

    Unsubscribe method - needs work.

--- partner_statement_email/controllers/subscriptions.py
+++ partner_statement_email/controllers/subscriptions.py
@@ -11,26 +11,26 @@ class SubscriptionManagement(http.Controller):
 
     @http.route('/subscribe/statement/<subscription_id>', type='http', auth='public', method='get')
     def po_search(self, subscription_id):
-        json_response = self._json_obj()
+        # json_response = self._json_obj()
         if isinstance(subscription_id, basestring):
             # Sanitize/remove non-alphanumeric characters, except dashes
             # po_number = re.sub(r'\W+', '', po_number)
             # zip = re.sub(r'\W+', '', zip)
             subscription_id = re.sub(r'[^\w-]|_', '', subscription_id)
 
-            subscription_id = http.request.env['scheduler.partner.statement'].sudo().search(subscription_id)
+            subscription = http.request.env['scheduler.partner.statement'].sudo().browse(int(subscription_id))
 
-            if len(subscription_id) > 0:
+            if len(subscription) > 0:
                 # If nothing was found, try again but capitalize the input.
-                subscription_id.active = False
+                subscription.active = False
 
                 return Response("You have been unsubscribed.",
-                                content_type='application/xhtml+xml;charset=utf-8',
-                                status=json_response['status'],
+                                content_type='application/html;charset=utf-8',
+                                status=200,
                                 headers=[('Cache-Control', 'public, max-age: 3600')])
 
         return Response("Unable to find your subscription",
-                        content_type='application/json;charset=utf-8',
+                        content_type='application/html+xml;charset=utf-8',
                         status=404,
                         headers=[('Cache-Control', 'public, max-age: 3600')])
 
